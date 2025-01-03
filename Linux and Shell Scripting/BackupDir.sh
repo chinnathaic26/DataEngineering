a8f0858f55af6448a274a70f0148c9bdbfd3737d
Chinnathai <97825771+chinnathaic26@users.noreply.github.com>
2025-01-03 22:45:29 +0530

    Update BackupDir.sh

--- Linux and Shell Scripting/BackupDir.sh
+++ Linux and Shell Scripting/BackupDir.sh
@@ -1,1 +1,11 @@
+To backup a directory,
+Source directory to destination directory
+
+Sour_DIR= "/source/path"
+Targ_DIR= "/target/path"
+Backup_FILE= "$Targ_DIR/backup-$date+%Y%m%d%h%m%s.tar.gz"
+echo "Create a backup"
+tar -czf "Backup_FILE" "Sour_DIR"
+echo " Backup Completed"
+
 
